1d67bb0fe9114ebae17474ab632f4b1a08e3a604
Ryo Yamada <r.yamada@variosecure.net>
2025-01-25 17:36:55 +0900

    fix: gen pdf workflow

--- build-pdf.js
+++ build-pdf.js
@@ -86,7 +86,13 @@ const outputFile = './docs/職務経歴書.pdf'; // 出力するPDFファイル
     `;
 
     // PuppeteerでPDFを生成
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: [
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--headless=new',
+      ]
+    });
     const page = await browser.newPage();
 
     // コンテンツ設定
